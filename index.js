27c05ad0cb7d1a66e9aa497ff65b005d79d712d0
evan <neff.evank@gmail.com>
2023-12-04 11:56:23 -0700

    small changes

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ const knex = require("knex")({
   client: "pg",
   connection: {
     host: process.env.DB_ENDPOINT || "localhost",
-    user: db_username || "postgres",
-    password: db_password || "mn4Fkv5qp8gcEne",
+    user: process.env.DB_USERNAME || "postgres",
+    password: process.env.DB_PASSWORD || "mn4Fkv5qp8gcEne",
     database: process.env.DB_NAME || "bucketlist",
     port: process.env.RDS_PORT || 5432,
     // ssl: process.env.DB_SSL ? { rejectUnauthorized: false } : false,
