38dc7442ac5a134b4862052aa733cfe3aa2e787c
evan <neff.evank@gmail.com>
2023-12-04 11:46:55 -0700

    Setting up basic website

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log("This is a test");
+const path = require("path");
+const express = require("express");
+const pg = require("pg");
+
+const app = express();
+app.set("view engine", "ejs");
+
+const port = process.env.PORT || 3000;
+app.use(express.urlencoded({ extended: true }));
+
+const knex = require("knex")({
+  client: "pg",
+  connection: {
+    host: process.env.DB_ENDPOINT || "localhost",
+    user: db_username || "postgres",
+    password: db_password || "mn4Fkv5qp8gcEne",
+    database: process.env.DB_NAME || "bucketlist",
+    port: process.env.RDS_PORT || 5432,
+    // ssl: process.env.DB_SSL ? { rejectUnauthorized: false } : false,
+  },
+});
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
